437e9cfe5469a0c82b7dfd72951babb36771e4a9
LonnieFowler <97270531+LonnieFowler@users.noreply.github.com>
2022-02-08 18:28:54 -0700

    load,copy,click and dblclick

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//Load
+window.onload = function (evt){
+    const heading = document.querySelector("h1");
+    heading.textContent = "READY TO GO!";
+   
+
+    //Copy
+    window.addEventListener("copy", () => {
+      navigator.clipboard.readText()
+      .then(text => {
+          heading.textContent += text 
+           }); 
+    });
+
+//Click
+document.body.addEventListener("click", evt => {
+    evt.target.classList.toggle('mirror')
+});
+
+//dblclick
+document.body.addEventListener("dblclick", evt => {
+    evt.target.outerHTML = ""
+});
+
+//
+
+
+
+}
+
